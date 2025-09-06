9e58e2dfe1529711086f9dd8ad013fba224182c0
liuqi008 <liuqi1613@gmail.com>
2025-09-07 03:11:33 +0800

    replace: upload local files to overwrite remote

--- core/src/IPC.cpp
+++ core/src/IPC.cpp
@@ -2,6 +2,7 @@
 #include "IPC.h"
 #include "WinUtils.h"
 #include <vector>
+#include <string>
 
 NamedPipeServer::NamedPipeServer(const std::wstring& pipe_name, Handler handler)
 : name_(pipe_name), handler_(std::move(handler)){}
@@ -25,8 +26,13 @@ void NamedPipeServer::run(std::atomic<bool>& running){
         }
 
         std::wstring resp;
-        try { resp = handler_? handler_(req) : L"{"ok":true}"; }
-        catch(...) { resp = L"{"ok":false}"; }
+        try {
+            if (handler_) resp = handler_(req);
+            else { resp.push_back(L'{'); resp.append(L"\"ok\":true"); resp.push_back(L'}'); }
+        }
+        catch(...) {
+            resp.clear(); resp.push_back(L'{'); resp.append(L"\"ok\":false"); resp.push_back(L'}');
+        }
 
         DWORD written=0;
         WriteFile(hPipe, resp.c_str(), (DWORD)((resp.size()+1)*sizeof(wchar_t)), &written, nullptr);
--- core/src/main.cpp
+++ core/src/main.cpp
@@ -17,7 +17,7 @@ static void logw(const std::wstring& s){
         wchar_t path[MAX_PATH]{};
         GetModuleFileNameW(nullptr, path, MAX_PATH);
         std::wstring p(path);
-        auto pos = p.find_last_of(L"\/");
+        auto pos = p.find_last_of(L"/\\"); // safe: no weird escapes
         if (pos != std::wstring::npos) p = p.substr(0,pos+1) + L"mirroraudio_core.log";
         else p = L"mirroraudio_core.log";
         log_file.open(p, std::ios::app);
@@ -39,20 +39,32 @@ int wmain(int argc, wchar_t** argv) {
     std::atomic<bool> running{ true };
     NamedPipeServer server(std::wstring(kPipeName), [&](const std::wstring& msg){
         // logging toggle
-        if (msg.find(L"\"log\"") != std::wstring::npos || msg.find(L""log"") != std::wstring::npos){
+        if (msg.find(L"\"log\"") != std::wstring::npos){
             if (msg.find(L"true") != std::wstring::npos) { log_enabled = true; logw(L"[log] enabled"); }
             else if (msg.find(L"false") != std::wstring::npos) { logw(L"[log] disabled"); log_enabled = false; }
-            std::wstring resp = L"{"ok":true,"log":"; resp += (log_enabled ? L"true" : L"false"); resp += L"}";
+            std::wstring resp;
+            resp.push_back(L'{');
+            resp.append(L"\"ok\":true,\"log\":");
+            resp.append(log_enabled ? L"true" : L"false");
+            resp.push_back(L'}');
             return resp;
         }
         // heartbeat
         if (msg.find(L"PING") != std::wstring::npos || msg.find(L"ping") != std::wstring::npos){
             logw(L"[ping] pong");
-            return std::wstring(L"{"ok":true,"pong":1}");
+            std::wstring resp;
+            resp.push_back(L'{');
+            resp.append(L"\"ok\":true,\"pong\":1");
+            resp.push_back(L'}');
+            return resp;
         }
         // other control
         engine.onControlMessage(msg);
-        return std::wstring(L"{"ok":true}");
+        std::wstring ok;
+        ok.push_back(L'{');
+        ok.append(L"\"ok\":true");
+        ok.push_back(L'}');
+        return ok;
     });
 
     std::thread pipeThread([&]{ server.run(running); });
